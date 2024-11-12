3a73af42bd49ca8a04de831aff332b6316e8e5ac
Othmane El Houssi <oelhoussi@wesleyan.edu>
2024-11-12 18:47:43 -0500

    Added authentification ton server.py

--- webserver/server.py
+++ webserver/server.py
@@ -1,6 +1,22 @@
-from flask import Flask
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
 
-app = Flask(__name__)
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+# Use the DB credentials you received by e-mail
+DB_USER = "oe2196"
+DB_PASSWORD = "A102030o"
+DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
+
+DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/w4111"
+
+# This line creates a database engine that knows how to connect to the URI above
+engine = create_engine(DATABASEURI)
+
 
 @app.route('/')
 def home():
